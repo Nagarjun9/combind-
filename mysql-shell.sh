49babf8a9725356521122d7054aab571dc95fddf
Nagarjun9 <nagunaggu@gmail.com>
2024-04-24 21:38:45 +0530

    combine

--- mysql-shell.sh
+++ mysql-shell.sh
@@ -19,11 +19,5 @@ VALIDATION $? "starting of mysql"
 
 #Below code will be useful for idempotent nature
 mysql -h db.daws78s.cloud -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
-if [ $? -ne 0 ]
-then 
-    mysql_secure_installation --set-root-pass ${mysql_root_password}
-    VALIDATION $? "mysql root password setup"
-else 
-   echo -e "$B mysql root password already set...SKIPPED $N"
-fi 
+ROOTPASSWORD
 
