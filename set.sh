d5d9fc45c80f6e43ed7909f09b3a520080320cd9
Nagarjun9 <nagunaggu@gmail.com>
2024-04-24 22:56:22 +0530

    combine

--- set.sh
+++ set.sh
@@ -1,7 +1,13 @@
 #!/bin/bash 
-set -e
+set -e  # this one to use any below commend error will come its exist that place 
+        # set -e not showing which line error come so we need use below trap commend to see which line error  
 
-trap  'linenumber ${LINENO} "$BASH_COMMAND"' ERR 
+linenumber(){             
+    echo "failed at $1: $2"
+}
+
+trap  'linenumber ${LINENO} "$BASH_COMMAND"' ERR  # this commend will show which line error 
+#linenumber is function name we can chage it, but ${LINENO} "$BASH_COMMAND"' ERR we cont change  
 
 USERID=$(id -u)
 
