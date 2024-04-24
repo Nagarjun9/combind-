11842053d9059529551e8b3159e9f2ce6bce3fd3
Nagarjun9 <nagunaggu@gmail.com>
2024-04-24 21:25:33 +0530

    combine

---
+++ common-shell.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAME=$(date +%F-%H-%M-%S)
+SCRITP_NAME=$(echo $0 |cut -d "." -f1)
+LOGFILE=/tmp/$SCRITP_NAME-$TIMESTAME.log 
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATION(){
+    if [ $1 -ne 0 ]
+    then 
+       echo -e "$Y $2...failed $N"
+    else 
+       echo -e "$B $2...success $N"
+}
+
+
+check_root(){
+       if [ $USERID -ne 0 ]
+       then 
+          echo -e " $R you are not a superuser please use root access $N"
+       else 
+          echo -e " $G you are a super user $N"
+       fi 
+}
--- mysql-shell.sh
+++ mysql-shell.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source ./common-shell.sh 
+
 check_root
 
 echo "please enter the password"
